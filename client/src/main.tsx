5effecfd50286ab8c8594db7f7d5822a136d9142
HyperLeverage <backpack.exhange@gmail.com>
2025-11-16 14:40:22 +0800

    Update main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,16 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { ThirdwebProvider } from "@thirdweb-dev/react";
-import { Base } from "@thirdweb-dev/chains";
+import { ThirdwebProvider } from "thirdweb/react";
 import App from "./App";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <ThirdwebProvider
-      activeChain={Base}
-      clientId={import.meta.env.VITE_THIRDWEB_CLIENT_ID}
-    >
+    <ThirdwebProvider>
       <App />
     </ThirdwebProvider>
   </React.StrictMode>
