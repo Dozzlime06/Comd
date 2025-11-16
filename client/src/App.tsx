6f1e116b2888c0150564d452113a7250233c6a3c
HyperLeverage <backpack.exhange@gmail.com>
2025-11-16 13:52:34 +0800

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,6 @@
 import { ThirdwebProvider } from "@thirdweb-dev/react";
 import { Base } from "@thirdweb-dev/chains";
-import { Web3Provider } from "@/contexts/web3context.tsx";  // ← ADD .tsx
+import { Web3Provider } from "@/contexts/web3context.tsx";
 import { Terminal } from "@/components/terminal";
 
 function App() {
