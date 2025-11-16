b763fa69285594ac53056806a3f8edec5ca9cc21
HyperLeverage <backpack.exhange@gmail.com>
2025-11-16 17:12:11 +0800

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,34 @@
+import { Switch, Route } from "wouter";
+import { queryClient } from "./lib/queryClient";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { Toaster } from "@/components/ui/toaster";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { ThirdwebProvider } from "thirdweb/react";
 import { Web3Provider } from "@/contexts/Web3Context";
-import { Terminal } from "@/components/Terminal";
+import Home from "@/pages/home";
+import NotFound from "@/pages/not-found";
+
+function Router() {
+  return (
+    <Switch>
+      <Route path="/" component={Home} />
+      <Route component={NotFound} />
+    </Switch>
+  );
+}
 
 function App() {
   return (
-    <Web3Provider>
-      <Terminal />
-    </Web3Provider>
+    <QueryClientProvider client={queryClient}>
+      <ThirdwebProvider>
+        <Web3Provider>
+          <TooltipProvider>
+            <Toaster />
+            <Router />
+          </TooltipProvider>
+        </Web3Provider>
+      </ThirdwebProvider>
+    </QueryClientProvider>
   );
 }
 
